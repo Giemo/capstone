8f042939c9d7d2cec67bb4275c0d4e0e86dfcb20
Giemo <romifollowme@gmail.com>
2019-05-11 13:30:48 +0000

    Don't use all images for traffic light classification

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -26,11 +26,7 @@ LABELS_NAME = [
 class TLClassifier(object):
     def __init__(self):
         model = os.path.join(os.path.dirname(os.path.abspath(__file__)), "frozen_inference_graph.pb")
-        assert os.path.exists(model), "model file not found at [%s]" % (model)
-
-        self.time_taken_for_inference = 0
         
-        # Read graph
         graph = tf.Graph()
         graph_def = tf.GraphDef()
         with open(model, 'rb') as f:
@@ -38,7 +34,6 @@ class TLClassifier(object):
         with graph.as_default():
             tf.import_graph_def(graph_def)
 
-        # set tensor holders
         self.image_tensor = graph.get_tensor_by_name("import/image_tensor:0")
         self.detection_boxes = graph.get_tensor_by_name("import/detection_boxes:0")
         self.detection_scores = graph.get_tensor_by_name("import/detection_scores:0")
@@ -48,7 +43,6 @@ class TLClassifier(object):
         config = tf.ConfigProto()
         config.graph_options.optimizer_options.global_jit_level = tf.OptimizerOptions.ON_1
 
-        # Load graph
         self.session = tf.Session(graph=graph, config=config)
 
     def get_classification(self, image):
@@ -59,27 +53,24 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
         """
         # preprocess image
-        image_ = cv2.resize(image, (300, 300))
-        image_ = cv2.cvtColor(image_, cv2.COLOR_BGR2RGB)
-        image_ = image_.astype(np.float32)
+        resized_image = cv2.resize(image, (300, 300))
+        rgb_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB)
+        np_image = rgb_image.astype(np.float32)
 
-        # traffic light detection
-        start_time = time.time()
         num_detections, classes, scores, boxes = self.session.run([self.num_detections, self.detection_classes, self.detection_scores, self.detection_boxes],
-                                                  feed_dict={self.image_tensor: np.expand_dims(image_, axis=0)})
-        self.time_taken_for_inference = time.time() - start_time
-        rospy.logdebug("Time taken for inference: %s" % (self.time_taken_for_inference))
+                                                  feed_dict={self.image_tensor: np.expand_dims(np_image, axis=0)})
+
+        num_detections = np.squeeze(num_detections).astype(np.uint32)
         classes = np.squeeze(classes)
         scores = np.squeeze(scores)
         boxes = np.squeeze(boxes)
-        num_detections = np.squeeze(num_detections).astype(np.uint32)
 
-        # identify type of signal (RED/GREEN/YELLOW)
         for i in range(num_detections):
-            class_idx = classes[i]
+            label_idx = classes[i]
 
             if scores[i] > 0.50:
-                rospy.loginfo("Identified Traffic Light: %s" % (LABELS_NAME[int(class_idx)]))
-                return LABELS[int(class_idx)]
-
+                rospy.loginfo("%s" % (LABELS_NAME[int(label_idx)]))
+                return LABELS[int(label_idx)]
+        
+        rospy.loginfo("UNKKNOWN")
         return TrafficLight.UNKNOWN
